c83e6df2cbfd84606aed4987542077308ce239b9
Ruslan Nurnazarov <ruslan7692@gmail.com>
2025-08-15 11:42:53 +0400

    add some tracing

--- src/main/java/com/example/javaorderservice/openfeign/FeignConfig.java
+++ src/main/java/com/example/javaorderservice/openfeign/FeignConfig.java
@@ -1,23 +1,23 @@
 package com.example.javaorderservice.openfeign;
-//
-//import io.micrometer.tracing.propagation.Propagator;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import io.micrometer.tracing.Tracer;
-//import feign.RequestInterceptor;
-//
-//@Configuration
-//public class FeignConfig {
-//
-//    @Bean
-//    public RequestInterceptor tracingFeignRequestInterceptor(Tracer tracer, Propagator propagator) {
-//        return template -> {
-//            if (tracer.currentSpan() != null) {
-//                // Автоматически добавляет W3C и B3
-//                propagator.inject(tracer.currentSpan().context(),
-//                        template,
-//                        (request, key, value) -> request.header(key, value));
-//            }
-//        };
-//    }
-//}
+
+import io.micrometer.tracing.propagation.Propagator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import io.micrometer.tracing.Tracer;
+import feign.RequestInterceptor;
+
+@Configuration
+public class FeignConfig {
+
+    @Bean
+    public RequestInterceptor tracingFeignRequestInterceptor(Tracer tracer, Propagator propagator) {
+        return template -> {
+            if (tracer.currentSpan() != null) {
+                // Автоматически добавляет W3C и B3
+                propagator.inject(tracer.currentSpan().context(),
+                        template,
+                        (request, key, value) -> request.header(key, value));
+            }
+        };
+    }
+}
